6fe2750b668c2a89ce3ae47ff0ddf4569ee2f16e
vildan1 <vildanakbulut98@gmail.com>
2023-04-30 21:09:12 +0300

    search review

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import { useGlobalContext } from '../../context';
 import './Search.css';
-import debounce from 'lodash/debounce';
 
 const Search = () => {
   const { setSearchTerm, setResultTitle, fetchNextPage } = useGlobalContext();
@@ -9,17 +8,7 @@ const Search = () => {
 
   useEffect(() => searchText.current.focus(), []);
 
-  const handleSearch = debounce(() => {
-    const tempSearchTerm = searchText.current.value.trim();
-
-    if (tempSearchTerm.replace(/[^\w\s]/gi, "").length === 0) {
-      setSearchTerm("");
-      setResultTitle("Please Enter Something ...");
-    } else {
-      setSearchTerm(searchText.current.value);
-    }
-  }, 500);
-
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     const tempSearchTerm = searchText.current.value.trim();
@@ -47,7 +36,7 @@ const Search = () => {
             <div className='searchText'> Name / Model </div>
 
             <div className='search-form-elem flex flex-sb bg-white'>
-              <input type='text' className='form-control' placeholder='Name/Model' ref={searchText} onChange={handleSearch} />
+              <input type='text' className='form-control' placeholder='Name/Model' ref={searchText}  />
 
             </div>
             <button type='submit' className='submitButton flex flex-c'>
